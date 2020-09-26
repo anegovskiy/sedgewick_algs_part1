c86a00433684038104d6004d641464561eb178d9
Andrew Negovskiy <a.negovskiy@gmail.com>
2020-09-26 13:11:29 +0300

    update problem kdtree with brute force solution

--- kdtree/src/KdTree.java
+++ kdtree/src/KdTree.java
@@ -6,17 +6,15 @@
 
 import edu.princeton.cs.algs4.Point2D;
 import edu.princeton.cs.algs4.RectHV;
-import edu.princeton.cs.algs4.StdDraw;
 
-import java.util.ArrayList;
 import java.util.TreeSet;
 
-public class PointSET {
+public class KdTree {
     private final TreeSet<Point2D> points;
 
 
     // construct an empty set of points
-    public PointSET() {
+    public KdTree() {
         points = new TreeSet<>();
     }
 
@@ -33,68 +31,41 @@ public class PointSET {
     // add the point to the set (if it is not already in the set)
     public void insert(Point2D p) {
         validatePoint(p);
+
         points.add(p);
     }
 
     // does the set contain point p?
     public boolean contains(Point2D p) {
         validatePoint(p);
+
         return points.contains(p);
     }
 
     // draw all points to standard draw
     public void draw() {
-        for (Point2D point : points) {
-            StdDraw.point(point.x(), point.y());
-        }
+
     }
 
     // all points that are inside the rectangle (or on the boundary)
     public Iterable<Point2D> range(RectHV rect) {
         validateRect(rect);
 
-        ArrayList<Point2D> containedPoints = new ArrayList<>();
-        for (Point2D point : points) {
-            if (rect.contains(point)) containedPoints.add(point);
-        }
-
-        return containedPoints;
+        Point2D minPoint = new Point2D(rect.xmin(), rect.ymin());
+        Point2D maxPoint = new Point2D(rect.xmax(), rect.ymax());
+        return points.subSet(minPoint, maxPoint);
     }
 
     // a nearest neighbor in the set to point p; null if the set is empty
     public Point2D nearest(Point2D p) {
         validatePoint(p);
-        Point2D nearest = null;
-        double distance = 0.0;
 
-        for (Point2D point : points) {
-            double newDistance = p.distanceTo(point);
-            if (distance == 0.0 || newDistance < distance) {
-                distance = newDistance;
-                nearest = point;
-            }
-        }
-
-        return nearest;
+        return points.first();
     }
 
     // unit testing of the methods (optional)
     public static void main(String[] args) {
-        Point2D firstP = new Point2D(0, 0);
-        Point2D secondP = new Point2D(1, 1);
-        Point2D thirdP = new Point2D(2, 2);
-        Point2D fourthP = new Point2D(3, 1);
 
-        PointSET pointSET = new PointSET();
-        pointSET.insert(firstP);
-        pointSET.insert(secondP);
-        pointSET.insert(thirdP);
-        pointSET.insert(fourthP);
-
-        System.out.printf("size: %d", pointSET.size());
-        System.out.println();
-        System.out.printf("nearest: %s", pointSET.nearest(fourthP).toString());
-        System.out.println();
     }
 
     // Validation
